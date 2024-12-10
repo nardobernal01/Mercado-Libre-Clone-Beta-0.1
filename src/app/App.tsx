8cd3f23547dac3c2138ac379f4159c42fa731129
nardobernal01 <nardobernal225@gmail.com>
2024-12-10 09:51:54 -0600

    Descripción de los cambios realizados2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 // App.tsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./home";
+
 import About from "./page";
 
 const App: React.FC = () => {
